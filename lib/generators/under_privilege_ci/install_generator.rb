7b6ed9a547ab65959490ed29a4b071955a292eeb
jasiekkk <jan.kamyk@netguru.pl>
2018-12-14 16:11:30 +0100

    minor style fix

--- lib/generators/under_privilege_ci/install_generator.rb
+++ lib/generators/under_privilege_ci/install_generator.rb
@@ -1,13 +1,13 @@
 module UnderPrivilegeCI
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
-      desc "Creates underprivilegeCI initializer for your application"
+      source_root File.expand_path('../../templates', __FILE__)
+      desc 'Creates underprivilegeCI initializer for your application'
 
       def copy_initializer
-        template "under_privilege_ci_initializer.rb", "config/initializers/under_privilege_ci.rb"
+        template 'under_privilege_ci_initializer.rb', 'config/initializers/under_privilege_ci.rb'
 
-        puts "Done!"
+        puts 'Done!'
       end
     end
   end
